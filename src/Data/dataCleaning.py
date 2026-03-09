bfb7b0b16c0d97ab9217e30d5377007192365668
Harishan Thilakanathan <harishan.thilak@gmail.com>
2026-03-09 02:36:12 -0400

    working on the data processin, should be done by end of tommorow

--- src/Data/dataCleaning.py
+++ src/Data/dataCleaning.py
@@ -3,8 +3,25 @@
 #Contributers: Harishan
 
 
+#calling pandas which is a Python library to process the csv data
 import pandas as pd
+
+#this for writing to json files
+import json
+
+#library for regular expressions
+import re
+
+
+#defining cosntants
+
+inputFile = ("OccupationsDataset.csv")
+
+outputFile = ("ProcessedOccupationsDataset.json")
+
 
 #Load the dataset
 df = pd.read_csv("OccupationsDataset.csv")
+
+
 
